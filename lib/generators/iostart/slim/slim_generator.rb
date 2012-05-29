5300c5772846831b33ded4f40228dd09e8a3c96f
Ignacio Ortega <ignacio.ortega@gmail.com>
2012-05-28 21:47:50 -0400

    run bundler on slim generator

--- lib/generators/iostart/slim/slim_generator.rb
+++ lib/generators/iostart/slim/slim_generator.rb
@@ -3,12 +3,16 @@ module Iostart
     class SlimGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
 
-      def add_gem
+      def install_gem
         gem 'slim-rails'
       end
 
       def add_application_template
         template "application.html.slim", "app/views/layouts/application.html.slim"
+      end
+
+      def run_bundler
+        run "bundle install"
       end
     end
   end
